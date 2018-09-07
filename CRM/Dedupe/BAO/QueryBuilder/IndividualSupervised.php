aa2baeb7d029908e1b82b60960fe8acfb51f3079
Jon Goldberg <jon@megaphonetech.com>
2018-09-07 15:52:44 -0400

    reserved dedupe - first/last OR email

--- CRM/Dedupe/BAO/QueryBuilder/IndividualSupervised.php
+++ CRM/Dedupe/BAO/QueryBuilder/IndividualSupervised.php
@@ -38,10 +38,10 @@ class CRM_Dedupe_BAO_QueryBuilder_IndividualSupervised extends CRM_Dedupe_BAO_Qu
                 SELECT contact.id as id1, {$rg->threshold} as weight
                 FROM civicrm_contact as contact
                   JOIN civicrm_email as email ON email.contact_id=contact.id
-                WHERE contact_type = 'Individual'
-                  AND first_name = %1
-                  AND last_name = %2
-                  AND email = %3", $params, TRUE),
+                WHERE contact_type = 'iIndividual'
+                  AND ((first_name = %1
+                    AND last_name = %2)
+                    OR email = %3)", $params, TRUE),
     );
   }
 
@@ -56,14 +56,16 @@ class CRM_Dedupe_BAO_QueryBuilder_IndividualSupervised extends CRM_Dedupe_BAO_Qu
     $query = self::filterQueryByContactList($rg->contactIds, "
             SELECT contact1.id as id1, contact2.id as id2, {$rg->threshold} as weight
             FROM civicrm_contact as contact1
-              JOIN civicrm_email as email1 ON email1.contact_id=contact1.id
               JOIN civicrm_contact as contact2 ON
                 contact1.first_name = contact2.first_name AND
                 contact1.last_name = contact2.last_name
-              JOIN civicrm_email as email2 ON
-                email2.contact_id=contact2.id AND
-                email1.email=email2.email
-            WHERE contact1.contact_type = 'Individual'");
+            WHERE contact1.contact_type = 'Individual'") . "UNION" .
+             self::filterQueryByContactList($rg->contactIds, "
+            SELECT contact1.id as id1, email2.contact_id as id2, {$rg->threshold} as weight
+            FROM civicrm_contact as contact1
+              JOIN civicrm_email email1 ON contact1.id = email1.contact_id
+              JOIN civicrm_email email2 ON email1.email = email2.email
+            WHERE contact1.contact_type = 'Individual'", 'contact1.id', 'email2.contact_id');
 
     return array(
       "civicrm_contact.{$rg->name}.{$rg->threshold}" => $query,
